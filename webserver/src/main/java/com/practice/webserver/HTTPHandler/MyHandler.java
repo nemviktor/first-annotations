e54f0485cf0e8178e8e05b0fc4081f1ebcc3523c
nemviktor <wookie7373@gmail.com>
2021-09-18 17:20:06 +0200

    Implementing basic routing get functionality.

--- webserver/src/main/java/com/practice/webserver/HTTPHandler/MyHandler.java
+++ webserver/src/main/java/com/practice/webserver/HTTPHandler/MyHandler.java
@@ -1,18 +1,38 @@
 package com.practice.webserver.HTTPHandler;
 
+import com.practice.webserver.HttpMEthods;
+import com.practice.webserver.Server.Server;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MyHandler implements HttpHandler {
 
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+    Server server;
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+
+        Method method = server.getRoutes().get(t.getHttpContext().getPath());
+        String response = null;
+        try {
+            response = (String) method.invoke(new Routes());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
+
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 }
--- webserver/src/main/java/com/practice/webserver/Server/Server.java
+++ webserver/src/main/java/com/practice/webserver/Server/Server.java
@@ -7,8 +7,11 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
 
 public class Server {
+
+    private HashMap<String, Method> routes = new HashMap<>();
 
     public void start() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
@@ -19,13 +22,19 @@ public class Server {
 
     private void creatCustomContext(HttpServer server) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Class classRoutes = Class.forName("com.practice.webserver.HTTPHandler.Routes");
-//        Object objRoutes = classRoutes.newInstance();
 
-        for(Method m: classRoutes.getMethods()) {
+        for(Method m: classRoutes.getDeclaredMethods()) {
             if(m.isAnnotationPresent(WebRoute.class)) {
                 String path = m.getAnnotation(WebRoute.class).path();
-                server.createContext(path, new MyHandler());
+                MyHandler myHandler = new MyHandler();
+                myHandler.setServer(this);
+                routes.put(path,m);
+                server.createContext(path, myHandler);
             }
         }
+    }
+
+    public HashMap<String, Method> getRoutes() {
+        return routes;
     }
 }
