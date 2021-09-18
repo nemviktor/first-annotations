fb73c61971bc75fd44ab00e4ab9db51c5b41736c
nemviktor <wookie7373@gmail.com>
2021-09-18 15:39:44 +0200

    Editing annotation path keys.

--- webserver/src/main/java/com/practice/webserver/HTTPHandler/Routes.java
+++ webserver/src/main/java/com/practice/webserver/HTTPHandler/Routes.java
@@ -4,12 +4,12 @@ import com.practice.webserver.CustomAnnotation.WebRoute;
 
 public class Routes {
 
-    @WebRoute("/test1")
+    @WebRoute(path="/test1")
     public String test1() {
         return "This is a msg from test1.";
     }
 
-    @WebRoute("/test2")
+    @WebRoute(path="/test2")
     public String test2() {
         return "This is a msg from test2.";
     }
