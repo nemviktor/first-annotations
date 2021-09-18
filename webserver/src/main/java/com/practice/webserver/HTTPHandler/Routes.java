ee14c11df8717898f77ab29357e165ce425d9fd7
nemviktor <wookie7373@gmail.com>
2021-09-18 15:36:23 +0200

    Annotating methods of Routes.

--- webserver/src/main/java/com/practice/webserver/HTTPHandler/Routes.java
+++ webserver/src/main/java/com/practice/webserver/HTTPHandler/Routes.java
@@ -1,4 +1,16 @@
 package com.practice.webserver.HTTPHandler;
 
+import com.practice.webserver.CustomAnnotation.WebRoute;
+
 public class Routes {
+
+    @WebRoute("/test1")
+    public String test1() {
+        return "This is a msg from test1.";
+    }
+
+    @WebRoute("/test2")
+    public String test2() {
+        return "This is a msg from test2.";
+    }
 }
