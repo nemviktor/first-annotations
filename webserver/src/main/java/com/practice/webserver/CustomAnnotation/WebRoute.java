bc574c5a6d30cc8266010b8ab8716b3aa6aca5ca
nemviktor <wookie7373@gmail.com>
2021-09-18 15:14:11 +0200

    Starter code. Creating first custom webroute annotation.

---
+++ webserver/src/main/java/com/practice/webserver/CustomAnnotation/WebRoute.java
@@ -0,0 +1,12 @@
+package com.practice.webserver.CustomAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface WebRoute {
+    String path() default "/";
+}
